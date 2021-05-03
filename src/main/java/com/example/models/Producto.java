d6d6fdfaaf638541082f9238119250858f350b55
jhorman1999 <jhormandaniel1999@gmail.com>
2021-05-03 16:08:44 -0500

    first commit

---
+++ src/main/java/com/example/models/Producto.java
@@ -0,0 +1,74 @@
+package com.example.models;
+
+import com.sun.istack.NotNull;
+import java.util.Calendar;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+import org.eclipse.persistence.nosql.annotations.DataFormatType;
+import org.eclipse.persistence.nosql.annotations.NoSql;
+
+
+
+@NoSql(dataFormat = DataFormatType.MAPPED)
+@Embeddable
+@XmlRootElement
+public class Producto {
+    
+    private String nombre;
+    private String marca;
+    private String fechaCompra;
+    
+    @NotNull
+    @Column(name = "create_at", updatable = false)
+    @Temporal(TemporalType.DATE)
+    private Calendar createdAt;
+    
+    @NotNull
+    @Column(name = "updated_at")
+    @Temporal(TemporalType.DATE)
+    private Calendar updatedAt;
+
+    public Producto(){
+    }
+    
+    @PreUpdate
+    private void updateTimestamp() {
+        this.updatedAt = Calendar.getInstance();
+    }
+
+    @PrePersist
+    private void creationTimestamp() {
+        this.createdAt = this.updatedAt = Calendar.getInstance();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getFechaCompra() {
+        return fechaCompra;
+    }
+
+    public void setFechaCompra(String fechaCompra) {
+        this.fechaCompra = fechaCompra;
+    }
+    
+    
+}
